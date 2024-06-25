e77ebd0ed10370f48f54e39b1782a68b7e7742c0
Vikash-27 <kandukuribalavikash.20.csd@anits.edu.in>
2024-06-25 21:02:35 +0530

    fixed firebase again

---
+++ src/utils/movieSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice({
+    name: "movie",
+    initialState: {
+        nowPlayingMovies:null
+    },
+    reducers: {
+        addNowPlayingMovies : (state, action) => {
+            state.nowPlayingMovies =  action.payload;
+        },
+        removeMovie: (state, action) => {
+            return null;
+        }
+    }
+});
+
+export const { addNowPlayingMovies } = moviesSlice.actions;
+
+export default moviesSlice.reducer;
