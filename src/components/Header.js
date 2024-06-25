eb48b41cc2ab678662a90acbfa4a0416d6f8f608
Vikash-27 <kandukuribalavikash.20.csd@anits.edu.in>
2024-06-25 20:40:19 +0530

    added tmdb api and changed OnAuthChanged code to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO, PROFILE_LOGO } from "../utils/constants";
 
 
 const Header = () => {
@@ -14,7 +15,7 @@ const Header = () => {
 
   useEffect(()=>{
     const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
   if (user) {
     
     const {uid,email,displayName} = user.uid;
@@ -31,6 +32,8 @@ const Header = () => {
   }
 });
 
+  return () => unsubscribe();
+
   },[])
   
   const handleSignOut = () => {
@@ -46,12 +49,12 @@ const Header = () => {
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
         alt="logo"
       />
       {user && (
         <div className="flex p-2">
-          <img className="w-12 h-12" alt="usericon" src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg" />
+          <img className="w-12 h-12" alt="usericon" src={PROFILE_LOGO} />
           <button onClick={handleSignOut} className="font-bold text-white">
             Sign Out
           </button>
---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const PROFILE_LOGO = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+
+export const LOGIN_BODY = "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2FlYjYwYWI5NzdkMWFjNGRhM2JlYzIyMWM2MDRhYSIsIm5iZiI6MTcxOTMxNzI1My43NjU5NDcsInN1YiI6IjY2N2FiMTNiZGQ3ODA5ZWE2YTE1OTczNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nqvCpxeCX9-zjQjMV5IZVwH-9T3FbWDLOjM0-bqOr9Y'
+    }
+  };
