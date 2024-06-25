c3e835423808754826bc55a3aeb167766f8169bd
Vikash-27 <kandukuribalavikash.20.csd@anits.edu.in>
2024-06-25 15:52:15 +0530

    modified firebase related files

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { firebaseConfigData } from "./firebaseCredentials";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBnUhAXIBdDNrp5ZhQTpnfiCTL5mB3YalE",
-  authDomain: "netflixgpt-a781c.firebaseapp.com",
-  projectId: "netflixgpt-a781c",
-  storageBucket: "netflixgpt-a781c.appspot.com",
-  messagingSenderId: "317247308320",
-  appId: "1:317247308320:web:d83227bdd9cec09aedb245",
-  measurementId: "G-GZ87SERHZK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
