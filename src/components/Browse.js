7705cb11695a86eeb2f912c8413165ef5d571652
Vikash-27 <kandukuribalavikash.20.csd@anits.edu.in>
2024-06-26 13:56:40 +0530

    deploy fixed

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import {addNowPlayingMovies} from "../utils/movieSlice";
+import { useDispatch } from "react-redux";
 
 const Browse = () => {
+  const dispatch = useDispatch();
   const nowPlayingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
     const json = await data.json();
     console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
   }
 
   useEffect(()=>{
