cf8672c12d7bc6f0cba107bec09a68d9d51e0ab1
Vikash-27 <kandukuribalavikash.20.csd@anits.edu.in>
2024-06-26 14:04:46 +0530

    fixed

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,24 +1,32 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
-import {addNowPlayingMovies} from "../utils/movieSlice";
+import { addNowPlayingMovies } from "../utils/movieSlice"; // Adjust the import path as per your file structure
 import { useDispatch } from "react-redux";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-  const nowPlayingMovies = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-    const json = await data.json();
-    console.log(json.results);
-    dispatch(addNowPlayingMovies(json.results));
-  }
+    const dispatch = useDispatch();
 
-  useEffect(()=>{
-    nowPlayingMovies();
-  },[])
+    const nowPlayingMovies = async () => {
+        try {
+            const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
+            const json = await data.json();
+            dispatch(addNowPlayingMovies(json.results));
+        } catch (error) {
+            console.error("Error fetching now playing movies:", error);
+        }
+    };
 
-    return <div>
-      <Header/>
-</div>;
-  };
-  export default Browse;
+    useEffect(() => {
+        nowPlayingMovies();
+    }, []);
+
+    return (
+        <div>
+            <Header />
+            {/* Add other components or UI elements */}
+        </div>
+    );
+};
+
+export default Browse;
--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -1,20 +1,23 @@
+// moviesSlice.js
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const moviesSlice = createSlice({
     name: "movie",
     initialState: {
-        nowPlayingMovies:null
+        nowPlayingMovies: null
     },
     reducers: {
-        addNowPlayingMovies : (state, action) => {
-            state.nowPlayingMovies =  action.payload;
+        addNowPlayingMovies: (state, action) => {
+            state.nowPlayingMovies = action.payload;
         },
         removeMovie: (state, action) => {
+            // Implement remove movie logic if needed
             return null;
         }
     }
 });
 
-export const { addNowPlayingMovies } = moviesSlice.actions;
+export const { addNowPlayingMovies, removeMovie } = moviesSlice.actions;
 
 export default moviesSlice.reducer;
